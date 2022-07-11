164d7bab32baa2caf97eb7e7e6beb0ad7c568ac1
diozenio <d.dionisiobsl@gmail.com>
2022-07-11 16:16:53 -0300

    Commentary deleted

--- src/screens/Authentication.tsx
+++ src/screens/Authentication.tsx
@@ -3,8 +3,6 @@ import { View } from "react-native";
 import styled from "styled-components/native";
 import { useNavigation } from "@react-navigation/native";
 
-// NEW COMENTARY
-
 const Authentication = () => {
   const navigation = useNavigation();
   return (

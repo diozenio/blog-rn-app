63aefd2aac9b1229f4b8945089aaa8178e99bf39
diozenio <d.dionisiobsl@gmail.com>
2022-07-11 10:28:40 -0300

    Routes

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const AuthRoutes = () => {
+  return <View style={{flex: 1, backgroundColor: "#333"}}/>;
+}
+
+export default AuthRoutes;

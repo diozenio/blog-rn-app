ff25a06619538ac24e29f36e534a22fe7c6241ee
diozenio <d.dionisiobsl@gmail.com>
2022-07-13 08:17:58 -0300

    feat: status bar configuration

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { ThemeProvider } from "styled-components";
+import { StatusBar } from "react-native";
 import { AuthProvider } from "./src/contexts/AuthContext";
 import theme from "./src/theme/theme";
 
 export default function App() {
   return (
     <AuthProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     <ThemeProvider theme={theme}>
       <Routes />
     </ThemeProvider>

cc465f192d8902fb61c68ac2c2aa49e41fa27567
diozenio <d.dionisiobsl@gmail.com>
2022-07-14 16:59:55 -0300

    chore: typings, hooks and the context splitted

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { createContext, useCallback, useMemo, useState } from "react";
+import React, { createContext, useCallback, useMemo, useState }  from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import uuid from "react-native-uuid";
 import { Alert } from "react-native";

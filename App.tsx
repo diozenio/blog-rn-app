20a5fba65a85831b91126306e2bcc46c3e311969
diozenio <d.dionisiobsl@gmail.com>
2022-07-14 22:15:03 -0300

    feat: fonts loading for custom fonts

--- App.tsx
+++ App.tsx
@@ -2,10 +2,19 @@ import { ThemeProvider } from "styled-components";
 import { StatusBar } from "react-native";
 import Routes from "./src/routes";
 import { AuthProvider } from "./src/contexts/AuthContext";
-import theme from "./src/theme/theme";
-import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  if (!loaded) {
+    return <ActivityIndicator animating={true} color={"blue"} />;
+  } else {
   return (
     <AuthProvider>
       <StatusBar
@@ -20,4 +29,5 @@ export default function App() {
       </ThemeProvider>
     </AuthProvider>
   );
+  }
 }

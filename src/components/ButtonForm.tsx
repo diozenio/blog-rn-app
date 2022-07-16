258bfccd2e4dacb6e293b448be09fba75d890603
diozenio <d.dionisiobsl@gmail.com>
2022-07-16 18:48:37 -0300

    feat: add button text as a prop

--- src/components/ButtonForm.tsx
+++ src/components/ButtonForm.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Button as B } from "react-native-paper";
+import { Button as B, Title } from "react-native-paper";
 import { IButtonForm } from "../shared/interfaces/ButtonForm";
 
-const ButtonForm: React.FC<IButtonForm> = ({ children, ...rest }) => {
-  return <Button {...rest} mode="outlined">{children}</Button>;
+const ButtonForm: React.FC<IButtonForm> = ({ buttonName, ...rest }) => {
+  return (
+    <Button uppercase={false} {...rest} mode="text">
+      <ButtonText>{buttonName}</ButtonText>
+    </Button>
+  );
 };
 
 export const Button = styled(B)`
   background-color: ${(props) => props.theme.colors.surface};
   padding-vertical: 5px;
   margin-top: 15px;
+`;
+
+export const ButtonText = styled(Title)`
+  color: ${(props) => props.theme.colors.text};
+  font-size: 18px;
+  text-align: center;
 `;
 
 export default ButtonForm;

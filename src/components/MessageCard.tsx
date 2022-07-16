0973dbc9a48ee4a69fb75e3856cda623ede06de9
diozenio <d.dionisiobsl@gmail.com>
2022-07-16 01:42:06 -0300

    feat: temporary message card style for posts

---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Paragraph, Title, withTheme } from "react-native-paper";
+
+const MessageCard = ({username, content} : any) => {
+  return (
+    <View style={{backgroundColor: '#333', marginBottom: 10}}>
+      <Title>{username}</Title>
+      <Paragraph>{content}</Paragraph>
+    </View>
+  );
+};
+
+export default MessageCard;

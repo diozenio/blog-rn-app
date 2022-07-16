3f8338240008ecb2945d5d0799e50d1ee47fca03
diozenio <d.dionisiobsl@gmail.com>
2022-07-16 01:55:17 -0300

    feat: temporary Home screen style

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,36 +1,87 @@
-import { View, Text, Button } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
-import React, { useEffect, useState } from "react";
+import React, {
+  useContext,
+  useMemo,
+} from "react";
+import {
+  Headline,
+  IconButton as IB,
+  Title,
+  withTheme,
+} from "react-native-paper";
+import styled from "styled-components/native";
+import MessageCard from "../components/MessageCard";
+import { PostsContext } from "../contexts/PostsContext";
 
-const Home = () => {
+interface Message {
+  id: string;
+  username: string;
+  content: string;
+}
+
+const Home = (props: any) => {
   const navigation = useNavigation();
-  const [messages, setMessages] = useState<FormData>([]);
-  async function handleFetchCards() {
-    try {
-      var response = await AsyncStorage.getItem("@blog:messages");
-      const data = response ? JSON.parse(response) : {};
-      setMessages(data);
-      console.log(messages);
-      
-    } catch (e) {
-      console.log("Erro no fetch");
-      console.log(e);
-    }
-  }
+  const { posts, fetchPosts } = useContext(PostsContext);
 
-  useEffect(() => {
-    handleFetchCards();
-  }, []);
-  return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        title="Tweet"
-        onPress={() => navigation.navigate("CreateMessage")}
+  const renderItem = (item: Message) => {
+    return (
+      <MessageCard
+        key={item.id}
+        username={item.username}
+        content={item.content}
       />
-    </View>
+    );
+  };
+
+  useMemo(() => {
+    fetchPosts();
+  }, [posts]);
+
+  return (
+    <SafeArea>
+      <Container>
+        <Headline>Home page</Headline>
+        <Title style={{ marginTop: 10 }}>Recent posts:</Title>
+        {posts.map((item: Message) => {
+          return renderItem(item);
+        })}
+      </Container>
+      <IconButton>
+        <IB
+          icon="message-square"
+          color={props.theme.colors.text}
+          size={30}
+          onPress={() => navigation.navigate("CreateMessage")}
+        />
+      </IconButton>
+    </SafeArea>
   );
 };
 
-export default Home;
+export const SafeArea = styled.View`
+  flex: 1;  
+  margin: 0;
+  padding 0;
+`;
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  padding: 30px;
+  padding-top: 40px;
+  z-index: 1;
+`;
+
+export const IconButton = styled.View`
+  background-color: ${(props) => props.theme.colors.surface};
+  height: 60px;
+  width: 60px;
+  z-index: 10;
+  border-radius: ${(props) => props.theme.roundness}px;
+  position: absolute;
+  bottom: 70px;
+  right: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default withTheme(Home);

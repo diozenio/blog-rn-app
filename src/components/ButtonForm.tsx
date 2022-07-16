66cd022f73ed92f675a3375ec4c9261733058596
diozenio <d.dionisiobsl@gmail.com>
2022-07-16 15:06:28 -0300

    types: ButtonForm type generics

--- src/components/ButtonForm.tsx
+++ src/components/ButtonForm.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 import { Button as B } from "react-native-paper";
+import { IButtonForm } from "../shared/interfaces/ButtonForm";
 
-const ButtonForm: React.FC = ({ children, ...rest }) => {
-  return (
-    <Button
-      mode="outlined"
-      {...rest}
-    >
-      {children}
-    </Button>
-  );
+const ButtonForm: React.FC<IButtonForm> = ({ children, ...rest }) => {
+  return <Button {...rest} mode="outlined">{children}</Button>;
 };
-
 
 export const Button = styled(B)`
   background-color: ${(props) => props.theme.colors.surface};
   padding-vertical: 5px;
   margin-top: 15px;
 `;
-
 
 export default ButtonForm;

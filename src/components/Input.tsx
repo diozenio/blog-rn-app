50d7c5fc62bb7dbc6b03c9aed8d28978748e8ec3
diozenio <d.dionisiobsl@gmail.com>
2022-07-16 18:48:53 -0300

    feat: input active color and label

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useImperativeHandle, useRef } from "react";
-import { TextInput as T } from "react-native-paper";
+import React, { useEffect, useImperativeHandle, useRef, useState } from "react";
+import { TextInput as T, Title } from "react-native-paper";
 import { useField } from "@unform/core";
 import { TextInputProps as TP } from "react-native-paper/lib/typescript/components/TextInput/TextInput";
 import styled from "styled-components/native";
+import { Alert } from "react-native";
 
 export interface TextInputProps extends Omit<TP, "theme"> {
   name: string;
@@ -15,7 +16,7 @@ interface InputRef {
 }
 
 const Input = React.forwardRef<InputRef, TextInputProps>(
-  ({ name, ...rest }, ref) => {
+  ({ name, label, ...rest }, ref) => {
     const { defaultValue, fieldName, registerField } = useField(name);
     const inputRef = useRef<InputValueReference>({ value: defaultValue });
     const inputElementRef = useRef<any>(null);
@@ -24,6 +25,9 @@ const Input = React.forwardRef<InputRef, TextInputProps>(
         inputElementRef.current.focus();
       },
     }));
+
+    const [activeColor, setactiveColor] = useState("#1b1b29");
+
 
     useEffect(() => {
       registerField<string>({
@@ -41,23 +45,38 @@ const Input = React.forwardRef<InputRef, TextInputProps>(
       });
     }, [fieldName, registerField]);
     return (
-      <StyledInput
-        ref={inputElementRef}
-        underlineColor={"transparent"}
-        defaultValue={defaultValue}
-        onChangeText={(value) => {
-          inputRef.current.value = value;
-        }}
-        autoComplete="off"
-        {...rest}
-      />
+      <>
+        <LabelText>{label}</LabelText>
+        <StyledInput
+          ref={inputElementRef}
+          underlineColor={"transparent"}
+          activeUnderlineColor={"transparent"}
+          defaultValue={defaultValue}
+          onFocus={() => setactiveColor("#1e1e2d")}
+          onBlur={() => setactiveColor("#1b1b29")}
+          style={{ backgroundColor: activeColor }}
+          onChangeText={(value) => {
+            inputRef.current.value = value;
+          }}
+          selectTextOnFocus={true}
+          selectionColor={"white"}
+          autoComplete="off"
+          {...rest}
+        />
+      </>
     );
   }
 );
 
 export const StyledInput = styled(T)`
+  width: 100%;
   margin-bottom: 15px;
   border-radius: ${(props) => props.theme.roundness}px;
+`;
+
+export const LabelText = styled(Title)`
+  font-size: 16px;
+  color: ${(props) => props.theme.colors.onSurface};
 `;
 
 export default Input;

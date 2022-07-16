c30f4e7d0b6424235ffc8f35842af258aa6d6010
diozenio <d.dionisiobsl@gmail.com>
2022-07-16 18:47:44 -0300

    types: update button form type

--- src/shared/interfaces/ButtonForm.ts
+++ src/shared/interfaces/ButtonForm.ts
@@ -1,5 +1,6 @@
-import { PropsWithChildren } from "./PropsWithChildren";
+// import { PropsWithChildren } from "./PropsWithChildren";
 
-export interface IButtonForm extends PropsWithChildren {
+export interface IButtonForm {
+  buttonName: string;
   onPress(): void | undefined;  
 }

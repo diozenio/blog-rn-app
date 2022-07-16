81b753005bcd08a999239bf6a0a54c17121b925f
diozenio <d.dionisiobsl@gmail.com>
2022-07-16 01:53:04 -0300

    feat: Posts provider and context api

--- src/routes/app.routes/index.tsx
+++ src/routes/app.routes/index.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../../screens/Home";
 import CreateMessage from "../../screens/CreateMessage";
+import PostsProvider from "../../contexts/PostsContext";
 
 const AppRouter = createNativeStackNavigator();
 
 const AppRoutes = () => {
   return (
-    <AppRouter.Navigator screenOptions={{ headerShown: false }}>
-      <AppRouter.Screen name="Home" component={Home} />
-      <AppRouter.Screen name="CreateMessage" component={CreateMessage} />
-    </AppRouter.Navigator>
+    <PostsProvider>
+      <AppRouter.Navigator screenOptions={{ headerShown: false }}>
+        <AppRouter.Screen name="Home" component={Home} />
+        <AppRouter.Screen name="CreateMessage" component={CreateMessage} />
+      </AppRouter.Navigator>
+    </PostsProvider>
   );
 };
 
